a0d0fa886f7a5775e3bd8266c804130a7df6b4e1
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2025-02-25 02:32:37 +0100

    Create basic_web_scraping.py

---
+++ basic_web_scraping.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+page_to_scrape = requests.get("http://quotes.toscrape.com")
+soup = BeautifulSoup(page_to_scrape.content, "html.parser")
+
+quotes = soup.find_all("span", attrs={"class": "text"})
+authors = soup.find_all("small", attrs={"class": "author"})
+
+for quote in quotes:
+    print(quote.text)
+
+for author in authors:
+    print(author.text)
